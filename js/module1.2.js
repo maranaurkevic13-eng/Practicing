60fde171c662898ac38dc2d76e35e7630b43430a
Маряна Юркевич <maranaurkevic13@gmail.com>
2026-03-16 21:42:41 +0200

    practicing

--- js/module1.2.js
+++ js/module1.2.js
@@ -110,3 +110,114 @@ function reverseEl(str) {
 };
 console.log(reverseEl('lol'));
 console.log(reverseEl('songs'));
+
+// TASK 12 ----------------------------------
+
+function sumElem(num1, num2) {
+    return num1 <= num2;
+};
+console.log(sumElem(2, 2));
+console.log(sumElem(2, 3));
+
+// TASK 13 ---------------------------
+
+function letterEl(str) {
+    if (!str) {
+        return false;
+    };
+
+    const firstLetter = str[0].toLowerCase();
+    const letter = ['a', 'e', 'i', 'o', 'u'];
+
+    return letter.includes(firstLetter); 
+};
+console.log(letterEl('Ania'));
+console.log(letterEl('Marianna'));
+
+// TASK 14 ------------------------------------
+
+function equalEl(num1, num2) {
+    return num1 === num2;
+};
+console.log(equalEl(5, 5));
+console.log(equalEl(6, 3));
+
+// TASK 15 ----------------------------
+
+function numdersElements(num) {
+    return num >= 0;
+};
+console.log(numdersElements(-1));
+console.log(numdersElements(5));
+
+// TASK 16 -------------------------------------
+function evenLine(str) {
+    return str.length % 2 === 0;
+};
+console.log(evenLine('lol'));
+console.log(evenLine('nine'));
+
+// TASK 17 ------------------------------------------
+
+function divisionNumber(num1, num2) {
+    const firstNumber = num1 % 2 === 0;
+    const secondNumber = num2 % 2 !== 0;
+    return firstNumber && secondNumber;
+};
+console.log(divisionNumber(4, 5));
+console.group(divisionNumber(5, 5));
+
+// TASK 18 --------------------------------------------
+
+function sumNumber(num) {
+    return num > 100 && num < 200;
+};
+console.log(sumNumber(150));
+console.log(sumNumber(210));
+
+// TASK 19 ------------------------------------------
+
+function letterElems(str) {
+    return str === str.toLowerCase(); 
+};
+console.log(letterElems('Books'));
+console.log(letterElems('songs'));
+
+// TASK 20 --------------------------------------------
+
+function letterString(str1, str2,) {
+    return str2.includes(str1);
+};
+console.log(letterString('cat', 'catalog'));
+console.log(letterString('red', 'blue'));
+
+// TASK 21 --------------------------------------
+
+function numberElem(num) {
+    return num < 0;
+};
+console.log(numberElem(-3));
+console.log(numberElem(3));
+
+// TASK 22 -------------------------------------
+
+function differenceEl(num1, num2) {
+    return num1 - num2 > 50;
+};
+console.log(differenceEl(200, 100));
+console.log(differenceEl(30, 15));
+
+// TASK 23 ------------------------------------
+
+function lastLetter(str) {
+    if (!str) {
+        return false;
+    };
+
+    const firstLetter = str[str.length -1].toLowerCase();
+    const letter = ['a', 'e', 'i', 'o', 'u'];
+
+    return letter.includes(firstLetter); 
+};
+console.log(lastLetter('natalia'));
+console.log(lastLetter('Marian'));
